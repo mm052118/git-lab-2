65650a15b2bd60a1ad9ab86f25eb2b92706fd15e
Matt McGreal <mm052118@ohio.edu>
2019-05-16 19:13:34 -0400

    first commit for git-lab-2, Hello Gitgit status

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Matt McGreal
+ *        Date: May 16 2019
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+	cout << "Hello Git!!" << endl;
+    return 0;
+}// main
